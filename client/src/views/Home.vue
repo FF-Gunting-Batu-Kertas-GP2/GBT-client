5b27fd4896be6186679ec51faa6ad48d9de6ec28
normndakbr <normnd.akbr2@gmail.com>
2020-12-11 09:17:26 +0700

    Updated Login form in client

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,26 @@
 <template>
   <div>
-    <h3>Home Page</h3>
-    <form @submit.prevent="sendName">
-      <input v-model="name" type="text" />
-      <button>Ok</button>
-    </form>
+    <div class="container">
+      <div class="login-form container mt-10">
+        <div class="row justify-content-center">
+          <div class="col-4 p-3 mt-5 shadow rounded">
+            <h1 class="mt-2" style="margin-bottom: 20px">GBT.io <small style="font-size=1px;">v2.0</small></h1>
+            <img class="m-5" src="../assets/logo.png" />
+            <form @submit.prevent="sendName" id="login-form">
+              <div class="form-group">
+                <input
+                  v-model="name"
+                  type="text"
+                  class="form-control"
+                  placeholder="Your Nickname"
+                />
+              </div>
+              <button type="submit" class="btn btn-success mt-4">Go</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
